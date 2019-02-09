bb4c1a986792061b4b399bedd2082513fe97dfea
sbfreddie <sbfreddie@comcast.net>
2019-02-09 15:31:28 -0800

    Initial Commit

---
+++ Master_Weather_Station/Master_Weather_Station.ino
@@ -0,0 +1,109 @@
+///
+/// @mainpage	Master_Weather_Station
+///
+/// @details	This is the Master Weather Station Code
+/// @n
+/// @n
+/// @n @a		Developed with [embedXcode+](https://embedXcode.weebly.com)
+///
+/// @author		freddie
+/// @author		Kingston Co.
+/// @date		2/9/19 3:31 PM
+/// @version	<#version#>
+///
+/// @copyright	(c) freddie, 2019
+/// @copyright	All rights reserved
+///
+/// @see		ReadMe.txt for references
+///
+
+
+///
+/// @file		Master_Weather_Station.ino
+/// @brief		Main sketch
+///
+/// @details	<#details#>
+/// @n @a		Developed with [embedXcode+](https://embedXcode.weebly.com)
+///
+/// @author		freddie
+/// @author		Kingston Co.
+/// @date		2/9/19 3:31 PM
+/// @version	<#version#>
+///
+/// @copyright	(c) freddie, 2019
+/// @copyright	All rights reserved
+///
+/// @see		ReadMe.txt for references
+/// @n
+///
+
+
+// Core library for code-sense - IDE-based
+// !!! Help: http://bit.ly/2AdU7cu
+#if defined(WIRING) // Wiring specific
+#include "Wiring.h"
+#elif defined(MAPLE_IDE) // Maple specific
+#include "WProgram.h"
+#elif defined(ROBOTIS) // Robotis specific
+#include "libpandora_types.h"
+#include "pandora.h"
+#elif defined(MPIDE) // chipKIT specific
+#include "WProgram.h"
+#elif defined(DIGISPARK) // Digispark specific
+#include "Arduino.h"
+#elif defined(ENERGIA) // LaunchPad specific
+#include "Energia.h"
+#elif defined(LITTLEROBOTFRIENDS) // LittleRobotFriends specific
+#include "LRF.h"
+#elif defined(MICRODUINO) // Microduino specific
+#include "Arduino.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(REDBEARLAB) // RedBearLab specific
+#include "Arduino.h"
+#elif defined(RFDUINO) // RFduino specific
+#include "Arduino.h"
+#elif defined(SPARK) || defined(PARTICLE) // Particle / Spark specific
+#include "application.h"
+#elif defined(ESP8266) // ESP8266 specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.0 and 1.5 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+// !!! Help http://bit.ly/2CL22Qp
+
+
+// Define structures and classes
+
+
+// Define variables and constants
+
+
+// Prototypes
+// !!! Help: http://bit.ly/2l0ZhTa
+
+
+// Utilities
+
+
+// Functions
+
+
+// Add setup code
+void setup()
+{
+
+}
+
+// Add loop code
+void loop()
+{
+
+}
---
+++ Master_Weather_Station/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  Master_Weather_Station
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project Master_Weather_Station
+  Created by freddie on 2/9/19
+  Copyright © 2019 Kingston Co.
+  Licence All rights reserved
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei Vilo, 2010-2019
+  All rights reserved
+  https://embedXcode.weebly.com
+
